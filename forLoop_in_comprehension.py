bd968c5e4d203f2e4f6cdd105439b3121c758f83
Perry <ch.parry1233@gmail.com>
2021-03-26 23:08:35 +0800

    Update forLoop_in_comprehension.py
    update

--- forLoop_in_comprehension.py
+++ forLoop_in_comprehension.py
@@ -21,4 +21,17 @@ matrix = [[0,1,2,3,4],
 matrix = [[column for column in range(0,5)] for row in range(0,5)]
 print(matrix)
 for each_column in matrix:
-    print(each_column) 
+    print(each_column)
+
+#TODO: sorting function
+new_name = [n for n in name if len(n)<=5]
+print(new_name)
+
+#TODO: if-else function
+#? if len(n)<=5 do nothing, else replace the string
+new_name2 = [n if len(n)<=5 else 'over 5 characters' for n in name]
+print(new_name2)
+
+#TODO: dictionary
+name_dict = {name.index(member) : member for member in name}
+print(name_dict)
